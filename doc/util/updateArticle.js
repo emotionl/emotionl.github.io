9d832d04c47e2415428e33f1fe62075d63b8170f
emotionl <emotionl@yeah.com>
2019-08-26 17:45:22 +0800

    feat:创建 初识Nginx Vim操作 Linux使用命令

--- doc/util/updateArticle.js
+++ doc/util/updateArticle.js
@@ -7,30 +7,32 @@ const readFileAsync = util.promisify(fs.readFile)
 const ARTICLE_CONFIG = {
     dirPath: path.resolve(__dirname, './../article'),
     outputPath: path.resolve(__dirname, './../00-Article.md'),
-    bridgePath: 'doc/article/'
+    bridgePath: 'doc/article/',
+    title: '文章'
 }
 
 const MATH_CONFIG = {
     dirPath: path.resolve(__dirname, './../math'),
     outputPath: path.resolve(__dirname, './../17-Math.md'),
-    bridgePath: 'doc/math/'
+    bridgePath: 'doc/math/',
+    title: '数学'
 }
 
 async function updateArticle(config) {
-    let { dirPath, outputPath, bridgePath } = config
+    let { dirPath, outputPath, bridgePath, title } = config
     let files = fs.readdirSync(dirPath)
     // let isFilesAlready = files.every(file => fs.statSync(articlePath + '/' + file).isFile())
     files.pop()
     let contentList = files.map(item => {
         let regex = /\d{3}\-(.+)\.md/
-        return item.replace(regex, (match, title) => {
-            return `- [${title}](${bridgePath}${match})`
+        return item.replace(regex, (match, pad) => {
+            return `- [${pad}](${bridgePath}${match})`
         })
     })
-    contentList.unshift('# 文章')
+    contentList.unshift(`# ${title}`)
     let content = contentList.join('\n\n')
     fs.writeFileSync(outputPath, content, 'utf8')
-    console.log('your data has been written!')
+    console.log(`${title} has been written!`)
 }
 // updateArticle(ARTICLE_CONFIG)
 updateArticle(MATH_CONFIG)
